831a79f54e1433a7a31a74318c3981729502478e
Austin Ruff <austinruff33@gmail.com>
2025-09-03 10:19:50 -0600

    week3 Bookwork

---
+++ week 3/src/Bookwork.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Bookwork {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+//		System.out.println();
+//		
+//		System.out.println("Type something:");
+//		String word = input.next();
+//		System.out.println(word);
+//		
+//		input.nextLine();
+//		
+//		System.out.println("Type something:");
+//		String word1 = input.next();
+//		System.out.println(word1);
+//		
+		
+//		final double CM_PER_INCH = 2.54;
+//		
+//		int inch = 100; 
+//		double cm = inch * CM_PER_INCH;
+//		
+		
+//		System.out.println("How many inches?");
+//		inch = input.nextInt();
+//	
+//		cm = inch * CM_PER_INCH;
+//		System.out.print(inch + " in = ");
+//		System.out.println(cm + " cm ");
+		
+//		System.out.printf("Four thirds = %.3f\n ", 4.0/3);
+//		System.out.printf("%d in = %.3f cm\n", inch, cm);
+//		
+//		String num = "73";
+//		double pi = 3.14;
+//				
+//		System.out.println((int)pi + " " + pi);
+//		System.out.println(Integer.parseInt(num) + pi);
+//		System.out.println(num + pi);
+//		
+//		int feet = 76/12;
+//		int inches = 76%12;
+//		
+//		System.out.println(feet + "' " + inches + "\"");
+		
+	double cm;
+	int feet, inches, remainder;
+	final int IN_PER_FOOT = 12;
+	final double CM_PER_inch = 2.54;
+	
+	System.out.println("Exactly how many centimeters?");
+	cm = input.nextDouble();
+	
+	inches = (int)(cm/CM_PER_inch);
+	feet = inches / IN_PER_FOOT;
+	remainder =  inches % IN_PER_FOOT;
+	System.out.printf("%.2f cm is %d ft and %d in\n", cm, feet, remainder);
+		
+		
+		
+	}
+	
+
+}
--- week 3/src/input_play.java
+++ week 3/src/input_play.java
@@ -2,87 +2,52 @@ import java.util.Scanner;
 
 public class input_play {
 
-	public static void main(String[] args) {
-		
-		java.util.Scanner input =  new Scanner(System.in);
-		
-		System.out.println(printInput()); //here we are calling a method with no parameters or returns
-		
-		
-		
-//		System.out.println("What is your name");
-//		
-//		String name = input.next();
-//		
-//		System.out.println("Great " + name + "!");
-//		
-//		System.out.println("Enter your age in years");
-//		
-//		int age = input.nextInt();
-		
-		System.out.println("");
-		
-		System.out.println("What is the temp in F");
-		
-		double fTemp = input.nextDouble(); //input for a floating point number doubble
-		
-		double cTemp = convertToC(fTemp);// calling a method that returns a double and takes a double as a param
-		
-		double kTemp = convertToK(cTemp);
-		
-	    System.out.println(fTemp + "F is " + cTemp + "C and " + kTemp + "K");
-		
-	    System.out.printf("%.2f F is %.2fC and %.2fK", fTemp, cTemp,kTemp);
-		
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-	}
-//public lets everyone use this
-//double is the data type of what is returned
-//double fTemp is the variable used inside this method it is a parameter/argument
-	public static double convertToC(double fTemp)
-	
-	{
-		return (fTemp - 32) *5/9;
-	}
-	
-	
-	
-	public static double convertToK(double fTemp)
-		
-		{
-		return cTemp + 273.15;
-	}
+        // Call the method that handles name and age
+        printInput(input);
 
-// private is the access static means its outside of main
-//void is the return type for returning nothing
-//input is the name and there are no perams
-private static void printInput() {
-System.out.println("What is your name");
+        System.out.println();
 
-String name = input.next();
+        System.out.println("What is the temp in F");
+        double fTemp = input.nextDouble(); 
 
-System.out.println("Great " + name + "!");
+        double cTemp = convertToC(fTemp);
+        double kTemp = convertToK(cTemp);
 
-System.out.println("Enter your age in years");
+        System.out.println(fTemp + "F is " + cTemp + "C and " + kTemp + "K");
+        System.out.printf("%.2f F is %.2fC and %.2fK\n", fTemp, cTemp, kTemp);
 
-int age = input.nextInt();
+        System.out.println("Enter Kilos");
+        double kilos = input.nextDouble();
+
+        double pounds = convertToPounds(kilos);
+        System.out.printf("%.2f kilos is %.2f pounds\n", kilos, pounds);
+
+        input.close();
+    }
+
+    private static double convertToPounds(double kilos) {
+        return kilos * 2.2;
+    }
+
+    public static double convertToC(double fTemp) {
+        return (fTemp - 32) * 5 / 9;
+    }
+
+    public static double convertToK(double cTemp) {
+        return cTemp + 273.15;
+    }
+
+    private static void printInput(Scanner input) {
+        System.out.println("What is your name");
+        String name = input.next();
+        System.out.println("Great " + name + "!");
+
+        System.out.println("Enter your age in years");
+        int age = input.nextInt();
+        System.out.println("You are " + age + " years old.");
+    }
 }
 
-
-
-
-
-
-
-{
-	
-
-int kilos;
-double kilos = 0;
-double kilos = convertToPounds(kilos);
-return kilos *2.2
-system.out.printf("%.2f", pounds)
-
-private static double convertToPounds(int kilos)
-}
